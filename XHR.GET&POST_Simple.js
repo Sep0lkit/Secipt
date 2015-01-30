b49e1e2156193a326ad9256270e930dc85eda387
KaiyiZhang <zky.own.star@gmail.com>
2015-01-31 00:33:09 +0800

    Add XHR.GET&POST to Repo

---
+++ XHR.GET&POST_Simple.js
@@ -0,0 +1,30 @@
+//The Variable
+var gurl = "$GURL";                             //The GET URL
+var purl = "$PURL";                             //The POST URL
+var selector="$SELECTOR";                       //The Selector for Query nonce value
+
+//Get Method for nonce value
+var xhr = new XMLHttpRequest();
+xhr.open('GET',gurl,'true');       
+
+var nonce;
+xhr.onreadystatechange = function(){
+
+if(xhr.readyState==4 && xhr.status==200){
+  var d;
+  d = document.createElement('div');
+  d.innerHTML = xhr.responseText;
+  nonce = d.querySelector(selector).value;
+  
+  //Get the nonce value and Post 
+  if(nonce != undefined){
+     var xhr2 = new XMLHttpRequest();
+     xhr2.open('POST',purl); 
+     xhr2.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+     var data="$DATA";                               //The Data Posted, This allways include the nonce value from GET
+     xhr2.send(data);
+    }
+  }
+}
+
+xhr.send();
---
+++ XHR.GET&POST_Simple.md
@@ -0,0 +1,19 @@
+# XHR.GET&POST_Simple 
+	--simple XHR GET&POSTER
+---
+
+####Usage:
+	Past the XHR.GET&POST_Simple.js to Chrome DevTools's Console, Replace the $VAR.
+
+####Description:
+		$GURL: 
+		--The GET Target Ex: /wp-admin/options-general.php	
+		
+		$PURL:
+		--The POST Target
+
+		$SELECTOR:
+		--The Selector for query nonce value
+
+		$DATA:
+		--The Post Data
