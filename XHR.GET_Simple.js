609da7c7758502a77391dbcd776c685276ed7f46
KaiyiZhang <zky.own.star@gmail.com>
2015-01-30 15:51:14 +0800

    Add XHR.GET_Simple to git

---
+++ XHR.GET_Simple.js
@@ -0,0 +1,12 @@
+var url = "$URL";                                        //The Target       
+var xhr = new XMLHttpRequest();
+xhr.open("GET",url,"$IsAsync[True|False]");              // Ture: Async
+
+xhr.onreadystatechange = function() {
+  if(xhr.readyState==4){
+       console.log('xhr:',xhr);
+       console.log('data:',xhr.responseText);
+  }
+}
+
+xhr.send();
