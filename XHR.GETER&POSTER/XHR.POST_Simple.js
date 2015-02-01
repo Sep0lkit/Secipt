d8ba47f041fcbf0e1d4a7b9254a422fb5ef4a4c3
KaiyiZhang <zky.own.star@gmail.com>
2015-02-01 15:08:48 +0800

    Repaire XHR.POST_Simple.js url error

--- XHR.GETER&POSTER/XHR.POST_Simple.js
+++ XHR.GETER&POSTER/XHR.POST_Simple.js
@@ -1,5 +1,5 @@
 var url =   "$URL";                             //The Target
 var xhr = new XMLHttpRequest();
-xhr.open('POST','$URL');               
+xhr.open('POST',url);               
 xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
 xhr.send("$DATA");                              //The Post Data 
